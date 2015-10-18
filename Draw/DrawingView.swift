b317a268cc8f2b2f359131f337e0adcf51d9ca5e
VitaminFloyd <VitaminFloyd@users.noreply.github.com>
2015-10-18 13:00:16 -0600

    Added files DrawingView.swift and ToolbarView.swift
    Project Settings:
    
    + numerous references to added files DrawingView.swift and ToolbarView.swift
    
    DrawingView.swift:
    
    + added file
    
    + (lines 13 - 20)			added comment defining scope of view
    
    ToolbarView.swift:
    
    + added file
    
    + (lines 13 - 20)			added comment defining scope of view

---
+++ Draw/DrawingView.swift
@@ -0,0 +1,31 @@
+//
+//  DrawingView.swift
+//  Draw
+//
+//  Created by Tom Bernard on 10/18/15.
+//  Copyright (c) 2015 Bersearch Information Services. All rights reserved.
+//
+
+import Cocoa
+
+/*
+
+A draw document will support drawing of oval, rectangle and line objects. Oval and Rectangle objects will
+maintain a stroke color and a fill color. Line objects will maintain a stroke color. All objects will maintain
+objectBounds
+
+A draw document will present a drawing view and a toolbar view (eventually, the toolbar view will move to a
+supporting panel instead of being instantiated within each document window).
+
+*/
+
+
+class DrawingView: NSView {
+
+    override func drawRect(dirtyRect: NSRect) {
+        super.drawRect(dirtyRect)
+
+        // Drawing code here.
+    }
+    
+}
