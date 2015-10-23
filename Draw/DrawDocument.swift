836467966acf88953296dfebf1034e2368cbe411
VitaminFloyd <VitaminFloyd@users.noreply.github.com>
2015-10-22 18:44:21 -0600

    + added
    /*
    
    Hillegass, A., Preble, A., Chandler, N.,
    Cocoa Programming for OS X, The Big Nerd Ranch Guide
    Pearson Technology Group, Indianapolis, Fifth Edition,
    first printing, April 2015
    Chapter 18, p 303, Challenge
    
    Draw
    
    */
    
    to
    
    AppDelegate.swift
    DrawDocument.swift
    DrawingView.swift
    ToolbarView.swift
    DrawObject.swift
    Oval.swift
    Rectangle.swift
    Line.swift

--- Draw/DrawDocument.swift
+++ Draw/DrawDocument.swift
@@ -6,6 +6,18 @@
 //  Copyright (c) 2015 Bersearch Information Services. All rights reserved.
 //
 
+/*
+
+Hillegass, A., Preble, A., Chandler, N.,
+Cocoa Programming for OS X, The Big Nerd Ranch Guide
+Pearson Technology Group, Indianapolis, Fifth Edition,
+first printing, April 2015
+Chapter 18, p 303, Challenge
+
+Draw
+
+*/
+
 import Cocoa
 
 
